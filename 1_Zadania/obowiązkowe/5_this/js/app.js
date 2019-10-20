903d2b141208173521682d758228361dc12292a2
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2019-10-20 22:39:50 +0200

    this - Rozwiazania zadan

---
+++ 1_Zadania/obowiązkowe/5_this/js/app.js
@@ -0,0 +1,38 @@
+//this - zadanie 0
+const car = {
+    brand: 'Mercedes',
+    color: 'Biały',
+    numberOfkilometers: 0,
+    printCarInfo: function(){
+        console.log(this.brand)
+        console.log(this.color)
+        console.log(this.numberOfkilometers)
+    },
+    drive: function(km){this.numberOfkilometers += km}
+}
+car.printCarInfo()
+car.drive(20)
+car.printCarInfo()
+
+//this - Zadanie 1
+car.technicalReview = ['2017-02-01','2018-02-01','2019-02-01']
+car.AddTechnicalReview = (review) => {
+    car.technicalReview.push(review)
+}
+
+console.log(car.technicalReview)
+car.AddTechnicalReview('2020-02-01')
+console.log(car.technicalReview)
+
+//this - zadanie 3
+const stairs = {
+    step: 0,
+    up: function () {this.step++},
+    down: function () {this.step--},
+    printStep: function () {console.log(this.step)}
+}
+stairs.up();
+stairs.up();
+stairs.up();
+stairs.down(); 
+stairs.printStep() // 2
--- 1_Zadania/obowiązkowe/5_this/js/zadanie02.js
+++ 1_Zadania/obowiązkowe/5_this/js/zadanie02.js
@@ -2,8 +2,15 @@ var calculator = {
     save: function (newA, newB) {
         this.a = newA;
         this.b = newB;
+    },
+    sum: function() {
+        let result = this.a + this.b
+        console.log(result)
+    },
+    multiply: function() {
+        let result = this.a * this.b
+        console.log(result)
     }
-
 };
 
 calculator.save(2, 3);
