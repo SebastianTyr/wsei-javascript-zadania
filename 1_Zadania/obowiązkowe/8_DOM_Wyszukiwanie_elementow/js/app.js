ae4cc685d6504d3d06eeb8c673de87327799cef1
Tyralski Sebastian <sebastian.tyralski@microsoft.wsei.edu.pl>
2019-12-08 10:45:56 +0100

    DOM_Wyszukiwanie_elementow - Rozwiazania zadan

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -1,46 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
     //Zadanie 0
-    var element = document.querySelector('.title')
-    function getDataAnimation (element){
-        var animation = document.getElementsByName('bounce')
-        console.log(animation)
+    var title = document.getElementsByClassName('title')[0];
+
+    function getDataAnimation(element){
+        var animation = element.getAttribute('data-animation')
         return animation
     }
-    getDataAnimation(element)
+    console.log(getDataAnimation(title))
 
     //Zadanie 1
     function getElements(){
         var home_el = document.getElementById('home')
-        var home_el1 = document.querySelector('#home')
-        var li_el = document.querySelector('li data-direction')
-        var block_el = document.querySelector('.block')
         console.log(home_el)
+        var home_el1 = document.querySelector('#home')
         console.log(home_el1)
+        var li_el = document.querySelector('ul li[data-direction]')
         console.log(li_el)
+        var block_el = document.querySelector('.block')
         console.log(block_el)
     }
     getElements()
 
     //Zadanie 2
     function getTags(){
-        var li_tag = document.getElementsByTagName('nav')
-        var paragraphs = document.querySelectorAll('div p')
-        var article_divs = document.querySelectorAll('article div')
+        var li_tag = document.querySelectorAll('nav li')
         console.log(li_tag)
+        var paragraphs = document.querySelectorAll('div p')
         console.log(paragraphs)
+        var article_divs = document.querySelectorAll('article div')
         console.log(article_divs)
     }
     getTags()
 
     //Zadanie 3
     function countTags(){
-        var counter = 0
-        var article_tags = document.querySelector('article .first') //TODO
-        for(let i = 0; i < article_tags.length; i++)
-        {
-            counter++
-        }
-        console.log(counter)
+        var article_tags = document.querySelector("article[class='first']")
+        var counter = article_tags.querySelectorAll('h1').length
+        return counter
     }
-    countTags()
+    console.log(countTags())
 });
