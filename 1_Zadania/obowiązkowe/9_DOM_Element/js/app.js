6a3069b55310836e90d88176ce4ab6c74bfe9869
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2019-12-09 16:39:17 +0100

    Element - zadania

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -4,4 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
     var banner = document.querySelector(".ban");
     var blocks = document.querySelectorAll(".block");
     var links = document.querySelector(".links").children;
+
+    function getDatasInfo(elements){
+        const retArray = []
+        retArray.forEach(element => {
+            retArray.push(elements) += element
+        });
+
+        return retArray
+    }
+    console.log(getDatasInfo(links))
 });
