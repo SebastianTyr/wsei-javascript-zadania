af2d05d247f79cabb4ed32a8e75c19003c2cf53d
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2019-12-11 09:28:20 +0100

    DOM_Element - zadania

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -4,8 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
     var banner = document.querySelector(".ban");
     var blocks = document.querySelectorAll(".block");
     var links = document.querySelector(".links").children;
+    //do zadania 3
+    var footer = document.getElementById("mainFooter");
 
-    function getDatasInfo(elements){
+    //Zadanie 0
+    /*function getDatasInfo(elements){
         const retArray = []
         retArray.forEach(element => {
             retArray.push(elements) += element
@@ -13,5 +16,43 @@ document.addEventListener("DOMContentLoaded", function () {
 
         return retArray
     }
-    console.log(getDatasInfo(links))
+    console.log(getDatasInfo(links))*/ //TODO
+
+    //Zadanie 1
+    function printData(){
+        console.log(homeElement)
+        
+        for(let counter = 0; counter < childElements.length; counter++){
+            console.log(counter)
+        }
+
+        console.log(banner)
+
+        for(let counter = 0; counter < blocks.length; counter++){
+            console.log(counter)
+        }
+
+        for(let counter = 0; counter < links.length; counter++){
+            console.log(counter)
+        }
+    }
+    printData()
+
+    //Zadanie 2
+    function changer(){
+        console.log(blocks.innerHTML)
+        console.log(blocks.outerHTML)
+        //Zwraca 'undefined' ponieważ wartości nie są ustawione żadną wartość
+
+        blocks.innerHTML = "Nowa wartość diva o klasie blocks"
+        console.log(blocks.innerHTML)
+        //Zwraca ustawioną wartość 'Nowa wartość diva o klasie blocks'
+    }
+    changer()
+
+    //Zadanie 3
+    function getID(element){
+        console.log(element)
+    }
+    getID(footer)
 });
