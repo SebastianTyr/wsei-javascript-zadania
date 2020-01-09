dc3a745044f23e4b6b1bba6c087cccdd86da7375
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2020-01-09 23:04:32 +0100

    Eventy - Rozwiazania zadan

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie09.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie09.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function innerFuncOne() {
       // Tutaj this wskazuje na element Window, bo funkcja została
       //wywołana bez żadnego kontekstu.
-      //b.style.backgroundColor = "green"
+      //Odpowiedź: b.style.backgroundColor = "green"
       console.log("innerFuncOne: ", this);
     }
     innerFuncOne();
