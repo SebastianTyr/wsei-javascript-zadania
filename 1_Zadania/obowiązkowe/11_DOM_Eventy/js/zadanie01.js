d0c2fae584c73ca0cd73cefb42f69828ad71a1b6
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2020-01-12 10:09:44 +0100

    Rozwiazania zadan

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
@@ -5,3 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
     menu.classList.add("menu");
     paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
 })
+//Po przeniesieniu tagu do sekcji <head> kod javascript nie zostaje wykonany poprawnie ze względu na załadowanie skryptu
+//przed całkmowitym załadowaniem zwartości strony
+//Rozwiązaniem jest napisanie skryptu wewnątrz eventu 'DOMContentLoaded', powoduje on
+//załadowanie skryptu dopiero po całkowitym załadowaniu zawartości strony
